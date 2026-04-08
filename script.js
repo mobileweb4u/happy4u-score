452f85ac52ed97e2b754a17e4295ef62687b9fc8
freddie <freddierussell4u@gmail.com>
2026-04-08 11:30:30 +0100

    Update app version from 3.8.2 to 4.0.0
    Update app version from 3.8.2 to 4.0.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let matchHistory = []; 
 let totalFramesPlayed = 0; 
 let activeScoringPlayer = null; 
-const APP_VERSION = "3.8.2"; 
+const APP_VERSION = "4.0.0"; 
 
 // Profiles added for the League Match Hall of Fame
 let playerProfiles = JSON.parse(localStorage.getItem('happy4u_profiles')) || {};
